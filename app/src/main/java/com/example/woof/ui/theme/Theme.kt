8c1ac134cb00f924db5f578b5ea187895231a7cb
Jeanpaul1111111111111111 <mukadjeanpaul@gmail.com>
2024-05-23 11:57:49 +0200

    Fin du projet

--- app/src/main/java/com/example/woof/ui/theme/Theme.kt
+++ app/src/main/java/com/example/woof/ui/theme/Theme.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MaterialTheme.shapes
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
@@ -236,6 +237,7 @@ fun WoofTheme(
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
+        shapes = shapes,
         content = content
     )
 }
